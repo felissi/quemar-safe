30bcb42b7c862d50a8e46f049891e2de06e62bd6
Felissi <felissistarry@gmail.com>
2023-01-24 19:43:50 +0800

    feat: title; og meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
-    <Header />
+  <Header />
   <NuxtPage></NuxtPage>
   <Footer />
 </template>
-<script>
-export default {}
+<script setup>
+import { useTagMeta, useHead } from '@unhead/vue'
+useHead({
+  title: 'Quemar'
+})
+useTagMeta([{
+  property: 'og:title',
+  content: 'Quemar'
+},
+{
+  property: 'og:description',
+  content: 'When it comes to outdoor barbecues during the covid pandemic, many people across the country have been staying home to prevent the spread of the virus. A great way to prevent the spread of germs during a barbecue is to avoid physical contact with the meat while it is cooking. Using condoms would help separate the raw meat and food.'
+},
+{
+  property: 'og:image',
+  content: '/og.png'
+}])
 </script>
